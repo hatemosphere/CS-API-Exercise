b9061e1573d12fe2a61bd822053535b88ee6711d
hatemosphere <hatemosphere@protonmail.com>
2019-02-17 18:40:27 +0200

    Making mongo host configurable for seeder

--- solution/mongo-seed/import-data.sh
+++ solution/mongo-seed/import-data.sh
@@ -3,10 +3,11 @@
 set -euo pipefail
 
 export MONGOIMPORT_CSV_FILE_FIELDS="survived.boolean(),passengerClass.int32(),name.string(),sex.string(),age.double(),siblingsOrSpousesAboard.int32(),parentsOrChildrenAboard.int32(),fare.double()"
+export MONGODB_URL=${MONGODB_URL:-"mongodb://localhost:27017/titanic"}
 
-while ! mongo mongo:27017/titanic --eval "db.version()" > /dev/null 2>&1; do sleep 1; done
+while ! mongo ${MONGODB_URL} --eval "db.version()" > /dev/null 2>&1; do sleep 1; done
 
-mongoimport --host mongo --db titanic --collection passengers --columnsHaveTypes \
+mongoimport --uri ${MONGODB_URL} --collection passengers --columnsHaveTypes \
     --fields ${MONGOIMPORT_CSV_FILE_FIELDS} \
     --type csv --file /mongo-seed/titanic.csv \
     --parseGrace skipRow
