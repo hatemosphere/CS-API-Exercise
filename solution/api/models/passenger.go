acfd833393c3fa9d3597120b4c14446762acb7cb
hatemosphere <hatemosphere@protonmail.com>
2019-02-18 13:42:44 +0200

    Adding error handling for most obvious errors

--- solution/api/models/passenger.go
+++ solution/api/models/passenger.go
@@ -10,12 +10,12 @@ const (
 
 type Passenger struct {
 	ID                      bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
-	Survived                bool          `json:"survived" bson:"survived" binding:"required"`
+	Survived                bool          `json:"survived" bson:"survived" binding:"exists"`
 	PassengerClass          int32         `json:"passengerClass" bson:"passengerClass" binding:"required"`
 	Name                    string        `json:"name" bson:"name" binding:"required"`
-	Sex                     string        `json:"sex" bson:"sex" binding:"required"`
+	Sex                     string        `json:"sex" bson:"sex,omitempty" binding:"required"`
 	Age                     float64       `json:"age" bson:"age" binding:"required"`
-	SiblingsOrSpousesAboard int32         `json:"siblingsOrSpousesAboard" bson:"siblingsOrSpousesAboard" binding:"required"`
-	ParentsOrChildrenAboard int32         `json:"parentsOrChildrenAboard" bson:"parentsOrChildrenAboard" binding:"required"`
+	SiblingsOrSpousesAboard int32         `json:"siblingsOrSpousesAboard" bson:"siblingsOrSpousesAboard" binding:"exists"`
+	ParentsOrChildrenAboard int32         `json:"parentsOrChildrenAboard" bson:"parentsOrChildrenAboard" binding:"exists"`
 	Fare                    float64       `json:"fare" bson:"fare" binding:"required"`
 }
